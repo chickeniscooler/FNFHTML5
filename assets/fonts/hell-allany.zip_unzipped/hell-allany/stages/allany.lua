9966c24a4ae9c03c643b07c77cef2aa1fa5e0886
chickeniscooler <154363935+chickeniscooler@users.noreply.github.com>
2024-01-25 15:29:35 -0500

    mod

---
+++ assets/fonts/hell-allany.zip_unzipped/hell-allany/stages/allany.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('stagefront', 'back', -900, -270);
+	setScrollFactor('stagefront', 1, 1);
+	scaleObject('stagefront', 1.05, 1.05);
+
+	addLuaSprite('stagefront', false);
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
